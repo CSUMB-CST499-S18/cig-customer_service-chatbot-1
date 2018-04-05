3d807960862a4a0e099c6e7a902962ffbaf44177
chanel <caquino@csumb.edu>
2018-04-04 22:47:15 -0700

    failed unit test

---
+++ Tests/setupProcess.spec.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const setupProcess = require('../Chatbot_Processor/SetupProcess_Intent/setupProcess.js');
+
+describe('FulfillmentCodeHook with no slots', () => {
+  it('Customer asks, \'how can I setup\'', done => {
+    const intentRequest = {
+      messageVersion: '1.0',
+      invocationSource: 'FulfillmentCodeHook',
+      sessionAttributes: null,
+      bot: {
+        name: 'CIG_Chatbot',
+        alias: null,
+        version: '$LATEST'
+      },
+      outputDialogMode: 'Text',
+      currentIntent: {
+        name: 'SetupProcess',
+        slots: { processTtype: null },
+        confirmationStatus: 'Confirmed'
+      },
+      inputTranscript: 'yes'
+    };
+
+    setupProcess(intentRequest).then(response => {
+      assert.equal(response.dialogAction.type, 'Close');
+      assert.equal(response.dialogAction.fulfillmentState, 'Fulfilled');
+      done();
+    });
+  });
+});
