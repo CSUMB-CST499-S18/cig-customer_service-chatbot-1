a83522238af904fd3556979351499c51bf0d8161
chanel <caquino@csumb.edu>
2018-04-04 23:55:51 -0700

    how is callback() not a function; saving for now; still errors

--- Tests/setupProcess.spec.js
+++ Tests/setupProcess.spec.js
@@ -2,7 +2,9 @@ const assert = require('chai').assert;
 const setupProcess = require('../Chatbot_Processor/SetupProcess_Intent/setupProcess.js');
 
 describe('FulfillmentCodeHook with no slots', () => {
-  it('Customer asks, \'how can I setup\'', done => {
+  it('Customer asks, \'how can I setup\'', () => {
+
+    // jest.setTimeout(15000);
     const intentRequest = {
       messageVersion: '1.0',
       invocationSource: 'FulfillmentCodeHook',
@@ -15,16 +17,19 @@ describe('FulfillmentCodeHook with no slots', () => {
       outputDialogMode: 'Text',
       currentIntent: {
         name: 'SetupProcess',
-        slots: { processType: null },
+        slots: { processType: 'auto' },
         confirmationStatus: 'Confirmed'
       },
       inputTranscript: 'yes'
     };
 
-    setupProcess(intentRequest).then(response => {
-      //assert.equal(response.dialogAction.type, 'Close');
-      assert.equal(response.dialogAction.fulfillmentState, 'Fulfilled');
-      done();
-    });
-  });
-});
+    // setupProcess(intentRequest).then(response => {
+    //   //assert.equal(response.dialogAction.type, 'Close');
+    //   assert.equal(response.dialogAction.fulfillmentState, 'Fulfilled');
+    //   done();
+    // });
+
+    const response = setupProcess(intentRequest);
+    assert.equal(response.dialogAction.fulfillmentState, 'Fulfilled');
+  }); // end it()
+}); // end describe(FulfillmentCodeHook with no slots)
