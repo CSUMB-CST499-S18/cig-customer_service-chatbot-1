181e1aeac66e3619feff2312b0a76eb117a02960
Andrew <druu831@gmail.com>
2018-05-09 02:07:37 +0000

    changes to RetrievePaymentInfo_Intent

--- Chatbot_Processor/RetrievePaymentInfo_Intent/manageFufillment.js
+++ Chatbot_Processor/RetrievePaymentInfo_Intent/manageFufillment.js
@@ -3,21 +3,32 @@
 const constants = require('../constant-vars');
 const lexResponses = require('../lexResponses');
 
+const obj = require('../mock-payload.json');
+
 function billDate() {
     var getDate = new Date();
     var month = getDate.getMonth() + 1;
     var day = constants.BILL_DAY;
     
-    return dueDate = month + "/" + day;
+    return month + "/" + day;
 }
 
-function billMin() {
-    var accountBalance = 1307;
-    var monthlyDue = 130;
-    var pastDue = 40;
-    var lateFee = 20;
-    
-    return MIN_DUE = MONTHLY_DUE+PAST_DUE+LATE_FEE;
+function extract(data) {
+   const bills = obj['billing']['accounts'];
+   for(var bill in bills) {
+     return bills[bill]['billingSummary'];
+   }
+}
+
+function billMin(obj) {
+  var accountBalance = obj.accountBalance;
+  var pastDue = parseFloat(obj.pastDue.substr(0,5));
+  if(pastDue>0) {
+    var lateFee = 20.00;
+  } else {
+    var lateFee = 0;
+  }
+  return pastDue+lateFee+130;
 }
 
 
@@ -25,9 +36,11 @@ module.exports = function(intentRequest, callback) {
     const sessionAttributes = intentRequest.sessionAttributes;
     const slots = intentRequest.currentIntent.slots;
     
+    var billingObj = extract(obj);
+    
     var MIN_DUE = constants.MONTHLY_DUE+constants.PAST_DUE+constants.LATE_FEE;
     var msg = "Hi, " + constants.FIRST_NAME + " " + constants.LAST_NAME 
-                + ", your current minimum due is " + billMin() + " is due on " + billDate()
+                + ", your current minimum due is " + billMin(billingObj) + " is due on " + billDate()
     
     callback(lexResponses.close(sessionAttributes, 'Fulfilled', {
         'contentType': 'PlainText',
